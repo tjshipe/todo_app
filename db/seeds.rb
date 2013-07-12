50f1aeff0605bdda3c7e2f1fa3c064578e40c8f8
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 09:16:49 -0500

    janky css fixes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@ User.create :name => 'Dev Bootcamp Student', :email => 'me@example.com', :passwo
 5.times do
   User.create :name => Faker::Name.name, :email => Faker::Internet.email, :password => 'password'
 end
+
+5.times do
+  User.first.lists.create name: Faker::Lorem.word
+end
+
+5.times do
+  User.first.lists.first.tasks.create name: Faker::Lorem.word
+end
